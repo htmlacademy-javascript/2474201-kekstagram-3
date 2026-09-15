de8745f55f47f884072590ec779d9194c6e37dba
Marina <m.g.vasyukova@gmail.com>
2026-09-15 21:49:14 +0300

    Исправляет семантику

--- js/functions.js
+++ js/functions.js
@@ -2,16 +2,16 @@
 
 const isLengthValid = (text, maxLength) => text.length <= maxLength;
 
-isLengthValid("проверяемая строка", 20); // true
-isLengthValid("проверяемая строка", 18); // true
-isLengthValid("проверяемая строка", 10); // false
+isLengthValid('проверяемая строка', 20); // true
+isLengthValid('проверяемая строка', 18); // true
+isLengthValid('проверяемая строка', 10); // false
 
 //Функция для проверки, является ли строка палиндромом.
 
 const isPalindrome = (text) => {
-  const normalizedText = text.replaceAll(" ", "").toLowerCase();
+  const normalizedText = text.replaceAll(' ', '').toLowerCase();
 
-  let reversedText = "";
+  let reversedText = '';
   for (let i = normalizedText.length - 1; i >= 0; i--) {
     reversedText += normalizedText[i];
   }
@@ -25,19 +25,19 @@ const isPalindrome = (text) => {
   return normalizedText === reversedText;
   }; */
 
-isPalindrome("топот"); // true
-isPalindrome("ДовОд"); // true
-isPalindrome("Кекс"); // false
-isPalindrome("Лёша на полке клопа нашёл "); // true
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
 
 const getNumberFromString = (text) => {
-  if (typeof text === "number") {
+  if (typeof text === 'number') {
     text = text.toString();
   }
 
-  let result = "";
+  let result = '';
   for (let i = 0; i < text.length; i++) {
     if (!Number.isNaN(parseInt(text[i]))) {
       result += text[i];
@@ -47,11 +47,11 @@ const getNumberFromString = (text) => {
   return parseInt(result, 10);
 };
 
-// console.log(getNumberFromString("2023 год")); // 2023
-// console.log(getNumberFromString("ECMAScript 2022")); // 2022
-// console.log(getNumberFromString("1 кефир, 0.5 батона")); // 105
-// console.log(getNumberFromString("агент 007")); // 7
-// console.log(getNumberFromString("а я томат")); // NaN
-// console.log(getNumberFromString(2023)); // 2023
-// console.log(getNumberFromString(-1)); // 1
-// console.log(getNumberFromString(1.5)); // 15
+getNumberFromString('2023 год'); // 2023
+getNumberFromString('ECMAScript 2022'); // 2022
+getNumberFromString('1 кефир, 0.5 батона'); // 105
+getNumberFromString('агент 007'); // 7
+getNumberFromString('а я томат'); // NaN
+getNumberFromString(2023); // 2023
+getNumberFromString(-1); // 1
+getNumberFromString(1.5); // 15
